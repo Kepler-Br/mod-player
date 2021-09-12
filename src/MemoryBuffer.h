4d1d990caa4a84bf4675df591437ffa2d64d8b47
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2021-09-12 20:42:38 +0300

    MemoryBuffer bugfixes

--- src/MemoryBuffer.h
+++ src/MemoryBuffer.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <streambuf>
+
 class MemoryBuffer : public std::basic_streambuf<char> {
+ private:
+  size_t _size;
+
  public:
-  MemoryBuffer(const char *p, size_t l) {
-    setg((char *)p, (char *)p, (char *)p + l);
-  }
+  MemoryBuffer(char *pointer, size_t size);
+
+  pos_type seekoff(off_type pos, std::ios_base::seekdir dir,
+                   std::ios_base::openmode mode) override;
 };
--- src/MemoryStream.h
+++ src/MemoryStream.h
@@ -7,7 +7,7 @@ class MemoryStream : public std::istream {
   MemoryBuffer _buffer;
 
  public:
-  MemoryStream(const char *p, size_t l)
+  MemoryStream(char *p, size_t l)
       : std::istream(&_buffer), _buffer(p, l) {
     rdbuf(&_buffer);
   }
